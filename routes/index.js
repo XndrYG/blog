78745defbfdea55f864d9e8a8ec544b9133668d8
XndrYG <xygarcia6406@gmail.com>
2022-03-29 08:44:03 -0700

    finished

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var express = require('express');
 var router = express.Router();
 const articleController = require('../controllers/articleController');
 const commentController = require('../controllers/commentController');
+const userController = require('../controllers/userController');
+const ensureUserAuthenticated = require('../middleware/ensureUserAuthenticated.js');
+const userHasRole = require('../middleware/userHasRole');
 /* GET home page. */
 router.get('/', function(req, res){
   res.redirect('/article');
@@ -10,14 +13,23 @@ router.get('/', function(req, res){
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/article/add', articleController.renderAddForm);
-router.post('/article/add',articleController.addArticle);
+router.get('/article/add', ensureUserAuthenticated, userHasRole('author'), articleController.renderAddForm);
+router.post('/article/add', ensureUserAuthenticated, userHasRole('author'), articleController.addArticle);
 router.get('/article/:articleId', articleController.displayArticle);
 router.get('/article/', articleController.displayAll);
-router.get('/article/:articleId/edit', articleController.renderEditForm);
-router.post('/article/:articleId/edit', articleController.updateArticle);
-router.get('/article/:articleId/delete', articleController.deleteArticle);
+router.get('/article/:articleId/edit', ensureUserAuthenticated, userHasRole('author'), articleController.renderEditForm);
+router.post('/article/:articleId/edit', ensureUserAuthenticated, userHasRole('author'), articleController.updateArticle);
+router.get('/article/:articleId/delete', ensureUserAuthenticated, articleController.deleteArticle);
 
 router.post('/article/:articleId/comment/create', commentController.createComment);
 router.post('/comment/:commentId/reply/create', commentController.addReply);
+
+router.get('/register', userController.renderRegistrationForm);
+router.post('/register', userController.register);
+router.get('/login', userController.renderLogin);
+router.post('/login', userController.login);
+router.get('/logout', userController.logout);
+
+router.get('/comment/:commentId/delete', ensureUserAuthenticated, userHasRole('admin'), commentController.deleteComment);
+router.get('/comment/:commentId/reply/:replyId/delete', ensureUserAuthenticated, userHasRole('admin'), commentController.deleteReply);
 module.exports = router;
